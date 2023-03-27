20e17e6803f9efd489008d85d01518a707510f0d
XyanZhang <1743067175@qq.com>
2023-03-27 11:37:33 +0800

    feat: initialize初始化，destroy断开连接

--- src/index.ts
+++ src/index.ts
@@ -36,10 +36,7 @@ let getUsers = async () => {
 // getUsers()
 
 
-
 let addPhoto = async () => {
-    await AppDataSource.initialize(); // 初始化链接
-    
     const photo = new Photo()
     photo.name = "Me and Bears"
     photo.description = "I am near polar bears"
@@ -55,4 +52,41 @@ let addPhoto = async () => {
     const savedPhotos = await AppDataSource.manager.find(Photo)
     console.log("All photos from the db: ", savedPhotos)
 }
-addPhoto()
+let useRepo = async () => {
+    const photoRepository = AppDataSource.getRepository(Photo)
+    const allPhotos = await photoRepository.find()
+    console.log("All photos from the db: ", allPhotos)
+
+    const firstPhoto = await photoRepository.findOneBy({
+        id: 1,
+    })
+    console.log("First photo from the db: ", firstPhoto)
+
+    const meAndBearsPhoto = await photoRepository.findOneBy({
+        name: "Me and Bears",
+    })
+    console.log("Me and Bears photo from the db: ", meAndBearsPhoto)
+
+    const allViewedPhotos = await photoRepository.findBy({ views: 1 })
+    console.log("All viewed photos: ", allViewedPhotos)
+
+    const allPublishedPhotos = await photoRepository.findBy({ isPublished: true })
+    console.log("All published photos: ", allPublishedPhotos)
+
+    const [photos, photosCount] = await photoRepository.findAndCount()
+    console.log("All photos: ", photos)
+    console.log("Photos count: ", photosCount)
+}
+
+
+let operateDb = async () => {
+    await AppDataSource.initialize(); // 初始化链接
+
+    // ... 数据库操作
+    // addPhoto()
+    await useRepo()
+
+    // 断开数据库连接
+    AppDataSource.destroy()
+}
+operateDb() 
